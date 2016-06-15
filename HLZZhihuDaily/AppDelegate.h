2bbcd6a204d2bc6d6d24aad4e03ee458ec375447
Hu Lizhen <hulizhen.public@gmail.com>
2016-06-15 23:02:52 +0800

    Initial Commit

---
+++ HLZZhihuDaily/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HLZZhihuDaily
+//
+//  Created by Hu Lizhen on 6/15/16.
+//  Copyright © 2016 hulizhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
